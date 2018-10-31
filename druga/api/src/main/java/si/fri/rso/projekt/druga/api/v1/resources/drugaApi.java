b2ae3e132269b84bb1ff40b363c7dd2ebd3eaffd
krneElL <susnikg@gmail.com>
2018-10-31 22:31:31 +0100

    Added Discovery support using Etcd

--- druga/api/src/main/java/si/fri/rso/projekt/druga/api/v1/resources/drugaApi.java
+++ druga/api/src/main/java/si/fri/rso/projekt/druga/api/v1/resources/drugaApi.java
@@ -1,18 +1,27 @@
 package si.fri.rso.projekt.druga.api.v1.resources;
 
 import com.kumuluz.ee.discovery.annotations.DiscoverService;
+import org.glassfish.jersey.process.internal.RequestScoped;
+
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@RequestScoped
 @Path("drugaTest")
 public class drugaApi {
 
     @Inject
     @DiscoverService(value = "druga")
     private WebTarget target;
+
+    @Inject
+    @DiscoverService(value = "prva")
+    private WebTarget target2;
 
     @GET
     @Produces("text/plain")
