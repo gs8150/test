482f7ff08fcce8e76c5929d784a4d22447494f9f
krneElL <susnikg@gmail.com>
2018-10-31 20:56:16 +0100

    Module druga added to TravisCL

--- prva/api/src/main/java/si/fri/rso/projekt/prva/api/v1/prvaApplication.java
+++ prva/api/src/main/java/si/fri/rso/projekt/prva/api/v1/prvaApplication.java
@@ -1,11 +1,11 @@
 package si.fri.rso.projekt.prva.api.v1;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-//@RegisterService
+@RegisterService
 @ApplicationPath("v1")
-
 public class prvaApplication extends Application {
 
 }
