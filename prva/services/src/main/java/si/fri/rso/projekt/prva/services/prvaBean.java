4fe41dac3111e685948bb1ebfb113c35556daabc
krneElL <susnikg@gmail.com>
2018-11-02 16:17:43 +0100

    Added Discovery support using Etcd (static)

--- prva/services/src/main/java/si/fri/rso/projekt/prva/services/prvaBean.java
+++ prva/services/src/main/java/si/fri/rso/projekt/prva/services/prvaBean.java
@@ -1,9 +1,11 @@
 package si.fri.rso.projekt.prva.services;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
 import java.util.logging.Logger;
 
 @ApplicationScoped
+@Dependent
 public class prvaBean {
 
     private Logger log = Logger.getLogger(prvaBean.class.getName());
